e68f61e57dcdb21860dfc7b93eaa028f82a72f01
KristinLind <kristin.lind@comcast.net>
2025-04-13 20:31:30 -0600

    project

---
+++ week07/ExerciseTracking/activity.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ExerciseTracking
+{
+    public abstract class Activity
+    {
+        private string _date;
+        private int _lengthInMinutes;
+
+        public Activity(string date, int lengthInMinutes)
+        {
+            _date = date;
+            _lengthInMinutes = lengthInMinutes;
+        }
+
+        public string GetDate() => _date;
+        public int GetMinutes() => _lengthInMinutes;
+
+        public abstract double GetDistance();
+        public abstract double GetSpeed();
+        public abstract double GetPace();
+
+        public virtual string GetSummary()
+        {
+            return $"{_date} {this.GetType().Name} ({_lengthInMinutes} min) - " +
+                   $"Distance: {GetDistance():0.0} miles, " +
+                   $"Speed: {GetSpeed():0.0} mph, " +
+                   $"Pace: {GetPace():0.0} min per mile";
+        }
+    }
+}
+
